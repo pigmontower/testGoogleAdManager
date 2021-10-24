cdeb226188957ce652fff94d4eb8753bbc56b8c6
山本真寛 <pigmontower@gmail.com>
2021-10-24 14:19:06 +0900

    Initial Commit

---
+++ testGoogleAdManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGoogleAdManager
+//
+//  Created by 山本真寛 on 2021/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
